da171657721d89c24d388e84cdd3df6bbdd9cf53
Natalia Paszkowska <npaszkowska@gmail.com>
2017-06-10 10:45:00 +0200

    commit

--- js/typydanych.js
+++ js/typydanych.js
@@ -7,3 +7,29 @@ var premia = 2500;
 var calkowitaWyplata;
 
 calkowitaWyplata = wyplata + premia;
+console.log(calkowitaWyplata);
+
+var wyplataStr = "5000";
+var premiaStr = "2500";
+var calkowitaWyplataStr;
+
+calkowitaWyplataStr = wyplataStr + ' "dodajemy"' + premiaStr;
+console.log(calkowitaWyplataStr);
+
+var imieStudenta = "    Michał ";
+console.log(imieStudenta.indexOf('c'));
+console.log(imieStudenta);
+console.log(imieStudenta.trim());
+
+/* typ logiczny */
+
+var czyJestSmog;
+czyJestSmog = true;
+
+if (czyJestSmog) {
+    console.log("Jest SMOG");
+} else {
+    console.log("Nie ma SMOGa ;)");
+}
+
+/* typy specjalne  */
